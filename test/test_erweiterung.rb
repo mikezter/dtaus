b15a4403605fe8dca43a5d581ad7a92308d63840
Torsten Mangner <tmangner@alphaone.de>
2011-05-22 13:24:09 +0200

    add unit tests for Erweiterung

---
+++ test/test_erweiterung.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.dirname(__FILE__) + "/test_helper")
+
+class TestErweiterung < Test::Unit::TestCase
+
+  def test_initialize
+    erw = Dtaus::Erweiterung.new(:kunde, 'text')
+    
+    assert erw
+    assert_equal '01', erw.type
+    assert_equal 27, erw.text.size
+    assert_equal 'TEXT                       ', erw.text
+  end
+
+  def test_initialize_incorrect_type
+    assert_raise( Dtaus::IncorrectErweiterungTypeException ) do
+      erw = Dtaus::Erweiterung.new(:fail, 'text')
+    end
+  end
+  
+  def test_initialize_incorrect_size
+    assert_raise( Dtaus::IncorrectSizeException ) do
+      erw = Dtaus::Erweiterung.new(:kunde, '1234567890123456789012345678')
+    end
+  end
+  
+  def test_from_string
+    erweiterungen = Dtaus::Erweiterung.from_string(:kunde, 'sehr langer text sehr langer text sehr langer text')
+    assert_equal 2, erweiterungen.size
+    assert_equal 'SEHR LANGER TEXT SEHR LANGE', erweiterungen[0].text
+    assert_equal 'R TEXT SEHR LANGER TEXT    ', erweiterungen[1].text
+
+    erweiterungen = Dtaus::Erweiterung.from_string(:kunde, 'kurzer text')
+    assert_equal 1, erweiterungen.size
+    assert_equal 'KURZER TEXT                ', erweiterungen[0].text
+
+    erweiterungen = Dtaus::Erweiterung.from_string(:kunde, 'längerer text mit ümläuten. ßÄÖÜ und trotzdem wird korrekt getrennt')
+    assert_equal 3, erweiterungen.size
+    assert_equal 'LAENGERER TEXT MIT UEMLAEUT', erweiterungen[0].text
+    assert_equal 'EN. SSAEOEUE UND TROTZDEM W', erweiterungen[1].text
+    assert_equal 'IRD KORREKT GETRENNT       ', erweiterungen[2].text
+  end
+
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+ROOT = File.join(File.dirname(__FILE__), '..')
+$LOAD_PATH << File.join(ROOT, 'lib')
+$LOAD_PATH << File.join(ROOT, 'lib', 'dtaus')
+
+require 'test/unit'
+require 'dtaus'
